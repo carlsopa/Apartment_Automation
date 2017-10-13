da6938339df24740121fe62f975b710c863112de
carlsopa <carlsonpar@gmail.com>
2017-10-12 22:08:38 -0400

    Update and rename test.py to ApartmentAutomation.py

--- ApartmentAutomation.py
+++ ApartmentAutomation.py
@@ -4,32 +4,24 @@ from Controlsa import *
 bl = Bedroom()
 cl = Chandelier()
 
-#bl = BedroomLights()
-
 app = Flask(__name__)
 app.secret_key = "my secret"
 @app.route('/')
 def Apartment():
     #return 'hello'
     return render_template('mobile.html', title='')
-
+#turn on/off bedroom lamps based on string value of "light"
 @app.route('/bedroomlight/<string:light>',methods=['POST'])
 def led(light):
-    #print(light)
     bl.Lights(light)
-    #if light == 0:
-    #    bl.Lights('left')
-    #elif light == 1:
-    #    bl.Lights('right')
     return('',204)
 
 @app.route('/chandelier')
 def Chandelier():
     return render_template('chandelier.html', title='')
-
+#control the living room chandelier based on the string value of "ctrl"
 @app.route('/chandelier/<string:ctrl>',methods=['POST'])
 def chandelier(ctrl):
-    #print('hello')
     cl.ChandelierControl(ctrl)
     return ('',204)
 
